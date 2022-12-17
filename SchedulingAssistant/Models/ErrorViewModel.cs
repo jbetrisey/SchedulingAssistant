a8e3c4c0ed8132d0f140b53b3c82cb5a13d19951
Julienne Bétrisey <julienne.betrisey@students.hevs.ch>
2022-12-17 12:26:50 +0100

    Add project files.

---
+++ SchedulingAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchedulingAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
