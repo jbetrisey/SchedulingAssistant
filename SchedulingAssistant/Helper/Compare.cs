ce01b6b47cde7e98d72242851974ecadef06e641
François Brouchoud <francois@brouchoud.ch>
2023-01-07 09:57:59 +0100

    feat: check compatible event with calendar

---
+++ SchedulingAssistant/Helper/Compare.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using SchedulingAssistant.Models;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+namespace SchedulingAssistant.Helper
+{
+    public class Compare
+    {
+        List<Activity> activities;
+        //activities = getFromCsv
+        List<MyCalendarEvent> myEvents;
+        //myEvents = getFromGoogle
+        List<CompatibleActivity> compatibleActivities;
+        /* 
+         foreach(Activity activity in activities){
+             foreach(MyCalendarEvent myCalendarEvent in myEvents){
+                 //tester que l'activité soit ok
+                    compatibleActivities.Add(activity);
+
+             }
+
+         }
+
+         */
+
+
+
+    }
+}
