3b4d8fa7e9231aa06dcaeb95d702ebd6b2b044d3
luca.delbuono <luca.delbuono@students.hevs.ch>
2023-01-07 10:37:56 +0100

    activity model

--- SchedulingAssistant/Models/Activity.cs
+++ SchedulingAssistant/Models/Activity.cs
@@ -7,7 +7,7 @@
         public int Day { get; set; }
         public int Month { get; set; }
         public int Year { get; set; }
-        public TimeSpan Time { get; set; }
+        public DateTime Time { get; set; }
         public string TeamHome { get; set; }
         public string TeamAway { get; set; }
     }
